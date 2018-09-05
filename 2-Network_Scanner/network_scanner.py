efb9015080800e9784c25ea074ab09981e018341
Lazaro Castro (Lazin) <lazarocastro@gmail.com>
2018-09-04 23:00:34 -0300

    Use ARP to ask who has target IP and Set destination MAC to broadcast MAC.

--- 2-Network_Scanner/network_scanner.py
+++ 2-Network_Scanner/network_scanner.py
@@ -3,7 +3,13 @@
 import scapy.all as scapy
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    arp_request.show()
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    broadcast.show()
+    arp_request_broadcast = broadcast/arp_request
+    arp_request_broadcast.show()
+
 
 scan("192.168.0.1/24")
     
