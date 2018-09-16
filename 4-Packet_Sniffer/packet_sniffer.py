ceaa105ea85bb4a57a21bfd7528df6bd89d0461f
Lazaro Castro (Lazin) <lazarocastro@gmail.com>
2018-09-16 20:24:29 -0300

    Extracting Fields From Layers

--- 4-Packet_Sniffer/packet_sniffer.py
+++ 4-Packet_Sniffer/packet_sniffer.py
@@ -7,7 +7,8 @@ def sniff(interface):
 
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        print(packet)
+       if packet.haslayer(scapy.Raw):
+           print(packet[scapy.Raw].load)
 
 sniff("wlp6s0")
 
